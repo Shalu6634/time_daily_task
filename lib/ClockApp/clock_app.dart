9bf2a03ae000f747ce38d7f41ffefffe1d1fa2a3
Shalu sharma <149373622+Shalu6634@users.noreply.github.com>
2024-05-02 11:51:23 +0530

    2-05

--- lib/ClockApp/clock_app.dart
+++ lib/ClockApp/clock_app.dart
@@ -1,4 +1,3 @@
-
 import 'dart:async';
 
 import 'package:clock_app/utils/global.dart';
@@ -19,110 +18,146 @@ class _ClockAppState extends State<ClockApp> {
       setState(() {
         dateTime = DateTime.now();
         if (dateTime.hour > 12) {
-          time = 'PM';
+          time = 'pm';
         } else {
-          time = 'AM';
+          time = 'am';
         }
         switch (dateTime.weekday) {
           case 1:
-            day = 'Monday';
+            day = 'Mon';
             break;
           case 2:
-            day = 'Tuesday';
+            day = 'Tue';
             break;
           case 3:
-            day = 'Wednesday';
+            day = 'Wed';
             break;
           case 4:
-            day = 'Thursday';
+            day = 'Thu';
             break;
           case 5:
-            day = 'Friday';
+            day = 'Fri';
             break;
           case 6:
-            day = 'Saturday';
+            day = 'Sat';
             break;
           case 7:
-            day = 'Sunday';
+            day = 'Sun';
+            break;
+        }
+
+        switch (dateTime.month) {
+          case 1:
+            month = 'Jan';
+            break;
+          case 2:
+            month = 'Feb';
+            break;
+          case 3:
+            month = 'March';
+            break;
+          case 4:
+            month = 'April';
+            break;
+          case 5:
+            month = 'May';
+            break;
+          case 6:
+            month = 'June';
+            break;
+          case 7:
+            month = 'July';
+            break;
+          case 8:
+            month = 'August';
+            break;
+          case 9:
+            month = 'Sept';
+            break;
+          case 10:
+            month = 'Oct';
+            break;
+          case 11:
+            month = 'Nov';
+            break;
+          case 12:
+            month = 'Dec';
             break;
         }
       });
     });
-    return Scaffold(
-      body: Align(
-        alignment: Alignment.center,
-        child: Container(
-          height: double.infinity,
-          width: double.infinity,
-          decoration: BoxDecoration(
-            color: Colors.black,
-            image: DecorationImage(
-              fit: BoxFit.cover,
-              image: AssetImage('assets/img/1.jpg'),
+    return SafeArea(
+      child: Scaffold(
+        body: Align(
+          alignment: Alignment.center,
+          child: Container(
+            height: double.infinity,
+            width: double.infinity,
+            decoration: BoxDecoration(
+              color: Colors.black,
+              image: DecorationImage(
+                fit: BoxFit.cover,
+                image: AssetImage('assets/img/1.jpg'),
+              ),
             ),
-          ),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-
-              Text(
-                '${dateTime.hour} : ${dateTime.minute} : ${dateTime.second}  $time',
-                style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 35,
-                    fontWeight: FontWeight.bold),
+            child: Padding(
+              padding: const EdgeInsets.only(top: 60),
+              child: Column(
+                children: [
+                  Text.rich(
+                    TextSpan(
+                      children: [
+                        TextSpan(
+                          text: '${dateTime.hour} : ${dateTime.minute}',
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 30,
+                              fontWeight: FontWeight.bold),
+                        ),
+                        TextSpan(
+                          text: '$time',
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Text.rich(
+                    TextSpan(
+                      children: [
+                        TextSpan(
+                          text: day,
+                          style: TextStyle(color: Colors.white, fontSize: 20,fontWeight: FontWeight.bold),
+                        ),
+                        TextSpan(
+                          text: ', ',
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold),
+                        ),
+                        TextSpan(
+                          text: '${dateTime.day} ',
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold),
+                        ),
+                        TextSpan(
+                          text: month,
+                          style: TextStyle(color: Colors.white, fontSize: 20,fontWeight: FontWeight.bold),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
               ),
-              Text(
-                '${dateTime.day} : ${dateTime.month} : ${dateTime.year} ',
-                style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 35,
-                    fontWeight: FontWeight.bold),
-              ),
-              Text(
-                day,
-                style: TextStyle(color: Colors.white, fontSize: 25),
-              ),
-            ],
+            ),
           ),
         ),
       ),
     );
   }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
