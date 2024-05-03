07fb35135186733fd81a0de624160559d8982aa5
Shalu sharma <149373622+Shalu6634@users.noreply.github.com>
2024-05-03 06:23:27 +0530

    Update clock_app.dart

--- lib/ClockApp/clock_app.dart
+++ lib/ClockApp/clock_app.dart
@@ -1,8 +1,8 @@
 import 'dart:async';
-
-import 'package:clock_app/utils/global.dart';
+import 'dart:math';
 import 'package:flutter/material.dart';
-// import 'package:time_daily_task/utils/global.dart';
+import '../utils/global.dart';
+
 
 class ClockApp extends StatefulWidget {
   const ClockApp({super.key});
@@ -17,7 +17,7 @@ class _ClockAppState extends State<ClockApp> {
     Timer.periodic(Duration(seconds: 1), (timer) {
       setState(() {
         dateTime = DateTime.now();
-        if (dateTime.hour > 12) {
+        if (dateTime.hour > 11) {
           time = 'pm';
         } else {
           time = 'am';
@@ -101,63 +101,146 @@ class _ClockAppState extends State<ClockApp> {
               ),
             ),
             child: Padding(
-              padding: const EdgeInsets.only(top: 110),
+              padding: const EdgeInsets.only(top: 60),
               child: Column(
                 children: [
                   Text.rich(
                     TextSpan(
-                      children: [
-                        TextSpan(
-                          text: '${dateTime.hour} : ${dateTime.minute}',
-                          style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 30,
-                              fontWeight: FontWeight.bold),
-                        ),
-                        TextSpan(
-                          text: '$time',
-                          style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 20,
-                              fontWeight: FontWeight.bold),
-                        ),
-                      ],
+                        children: [
+                    TextSpan(
+                    text:
+                    '${dateTime.hour % 12} : ${dateTime.minute} : ${dateTime.second}',
+                        style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 30,
+                        fontWeight: FontWeight.bold),
+                  ),
+                  TextSpan(
+                    text: '$time',
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold),
+                  ),
+                ],
+              ),
+            ),
+            Text.rich(
+              TextSpan(
+                children: [
+                  TextSpan(
+                    text: day,
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold),
+                  ),
+                  TextSpan(
+                    text: ', ',
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold),
+                  ),
+                  TextSpan(
+                    text: '${dateTime.day} ',
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold),
+                  ),
+                  TextSpan(
+                    text: month,
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 40,
+            ),
+            Container(
+              height: 190,
+              width: 190,
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.white, width: 5),
+                shape: BoxShape.circle,
+              ),
+              child: Stack(
+                alignment: Alignment.center,
+                children: [
+                  Transform.rotate(
+                    angle: (dateTime.hour * 30 * pi) / 180,
+                    child: const Center(
+                      child: VerticalDivider(
+                        thickness: 3,
+                        color: Colors.red,
+                        indent: 50,
+                        endIndent: 80,
+                      ),
                     ),
                   ),
-                  Text.rich(
-                    TextSpan(
-                      children: [
-                        TextSpan(
-                          text: day,
-                          style: TextStyle(color: Colors.white, fontSize: 20,fontWeight: FontWeight.bold),
+                  ...List.generate(60, (index) =>
+                      Transform.rotate(
+                        angle: ((index )* 6 * pi) / 180,
+                        child: ((index + 1) % 5 == 0) ?
+                        VerticalDivider(
+                          thickness: 2,
+                          color: Colors.red,
+                          indent: 0,
+                          endIndent: 160,
+                        )
+                            :
+                        VerticalDivider(
+                          thickness: 2,
+                          color: Colors.grey,
+                          indent: 0,
+                          endIndent: 170,
                         ),
-                        TextSpan(
-                          text: ', ',
-                          style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 20,
-                              fontWeight: FontWeight.bold),
-                        ),
-                        TextSpan(
-                          text: '${dateTime.day} ',
-                          style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 20,
-                              fontWeight: FontWeight.bold),
-                        ),
-                        TextSpan(
-                          text: month,
-                          style: TextStyle(color: Colors.white, fontSize: 20,fontWeight: FontWeight.bold),
-                        ),
-                      ],
+                      ),
+                  ),
+                  Center(
+                    child: Container(
+                      height: 8,
+                      width: 8,
+                      decoration: BoxDecoration(
+                          color: Colors.black, shape: BoxShape.circle),
+                    ),
+                  ),
+                  Transform.rotate(
+                    angle: (dateTime.second * 6 * pi) / 180,
+                    child: const Center(
+                      child: VerticalDivider(
+                        thickness: 2,
+                        color: Colors.red,
+                        indent: 25,
+                        endIndent: 80,
+                      ),
+                    ),
+                  ),
+                  Transform.rotate(
+                    angle: (dateTime.minute * 6 * pi) / 180,
+                    child: const Center(
+                      child: VerticalDivider(
+                        thickness: 3,
+                        color: Colors.black,
+                        indent: 32,
+                        endIndent: 80,
+                      ),
                     ),
                   ),
                 ],
               ),
             ),
+            ],
           ),
         ),
       ),
+    ),)
+    ,
     );
   }
 }
